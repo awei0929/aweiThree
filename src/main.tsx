82ce90997c213e4a2233b78195fa2aff97a40576
awei <awei0929@sjzn.com>
2025-01-13 15:23:17 +0800

    feat(修复): 调整

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PageLayout from './layout/page-layout';
-import Login from './pages/login/login';
 import { useToken } from '@/hooks/useToken';
 import store from './redux';
 // import { getAccountInfo } from '@/redux/actionCreators';
@@ -16,17 +15,6 @@ function App() {
       // store.dispatch(getAccountInfo());
     }
   }, []);
-
-  /**
-   * 获取未登录状态的route
-   * @returns
-   */
-  const getUnLoginRoute = () => (
-    <Routes>
-      <Route path="/sso" element={<Login />} />
-      <Route path="*" element={<Navigate to="/sso" />} />
-    </Routes>
-  );
 
   /**
    * 获取已登录状态的route
